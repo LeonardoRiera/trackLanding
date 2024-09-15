da0ecd7fd3d9cdde661a5ae438d1b9c26413ab9a
LeonardoRiera <159967149+LeonardoRiera@users.noreply.github.com>
2024-09-15 00:09:16 -0300

    trabajo en segunda seccion about us

--- main.js
+++ main.js
@@ -7,3 +7,21 @@ gsap.to('#textoPrincipal', {
     y:200 
     
 });
+
+
+
+const imagen = document.getElementById('miMontaña');
+
+const observer = new IntersectionObserver((entries) => {
+  const entry = entries[0]; // Solo necesitamos observar la primera entrada (nuestra imagen)
+  
+  if (entry.isIntersecting) {
+    entry.target.classList.add('visible'); // Agregar la clase visible cuando el elemento entra en el viewport
+  } else {
+    entry.target.classList.remove('visible'); // Remover la clase visible cuando el elemento sale del viewport
+  }
+}, {
+  threshold: 0.1 // La animación se activa cuando al menos el 10% del elemento es visible
+});
+
+observer.observe(imagen); // Observamos la imagen
